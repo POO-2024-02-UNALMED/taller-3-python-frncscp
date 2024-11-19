d931088cacfe4ff486a2de0eef548d72bb0b7e8f
frncscp <franciscocerenporto@gmail.com>
2024-11-18 23:15:18 -0500

    fix: line in enlazar

--- televisores/control.py
+++ televisores/control.py
@@ -11,7 +11,7 @@ class Control:
 
     def enlazar(self, tv):
         self.tv = tv
-        tv.setControl(self)
+        self.tv.setControl(self)
 
     def turnOn(self):
         self.tv.estado = True
